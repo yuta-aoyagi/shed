be9002a063557645fcac0422af8ac1da2f94bd89
Yuta Aoyagi <yuta-aoyagi@users.noreply.github.com>
2021-02-03 14:04:55 +0900

    Import initial add.sh

---
+++ add.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set +u
+profile=${1?missing profile name}
+creds_file=${AWS_SHARED_CREDENTIALS_FILE-~/.aws/credentials}
+set -u
+
+do_add() {
+  read -r id &&
+    read -r secret &&
+    cat <<EOS >>"$creds_file"
+
+[$profile]
+aws_access_key_id = $id
+aws_secret_access_key = $secret
+EOS
+}
