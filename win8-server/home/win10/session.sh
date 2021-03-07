f2eb64d3eb8a7c1a92b63b3d6790afb6f9cd3ac1
Yuta Aoyagi <yuta-aoyagi@users.noreply.github.com>
2021-03-07 14:31:46 +0900

    Add home/win10/session.sh, to which the key in authorized_keys is binded
    home/.ssh/authorized_keys was modified at 2021-03-07 14:31:46 +0900.
    home/win10/session.sh was modified at 2021-03-07 14:31:16 +0900.

---
+++ win8-server/home/win10/session.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+set -u
+
+find_token() {
+  # shellcheck disable=SC2006
+  fresh=`find "$token" -mmin 0`
+}
+
+msg() {
+  printf '%s\n' "$1" >&2
+  false
+}
+
+consume() {
+  token=win10/$1
+  if find_token; then
+    if [ "$fresh" ]; then
+      rm "$token" || msg "shouldn't reach here"
+    else
+      msg "you don't have one-time token"
+    fi
+  else
+    msg "shouldn't reach here"
+  fi
+}
+
+do_work() {
+  case $SSH_ORIGINAL_COMMAND in
+    get) consume get && cat /dev/clipboard ;;
+    put) consume put && cat >/dev/clipboard ;;
+    *) msg "unknown command: $SSH_ORIGINAL_COMMAND"
+  esac
+}
+
+ts_rb() {
+  ruby --disable gems /etc/opt/openssh/ts.rb
+}
+
+# shellcheck disable=SC2006
+log_file=win10/`date -u +%Y-%m-%d`.log
+{ do_work 2>&1 >&5 5>&- | ts_rb 5>&- | tee -a "$log_file" >&2; } 5>&1
