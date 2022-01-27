96361b74e2dc2cd00c97fe4fe509279a5eeb6eb4
Yuta Aoyagi <yuta-aoyagi@users.noreply.github.com>
2022-01-27 22:04:29 +0900

    Make mingw.sh propagate non-zero exit code
    Prior to this commit, it sometimes didn't.
    
    try-qemu/mingw.sh was modified at 2021-12-30 01:58:12.265241000 +0900.

--- try-qemu/mingw.sh
+++ try-qemu/mingw.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-PATH=${PATH##*bin:}:/usr/x86_64-w64-mingw32/sys-root/mingw/bin
-"$@"
+set -u
+
+(
+  PATH=${PATH##*bin:}:/usr/x86_64-w64-mingw32/sys-root/mingw/bin
+  "$@"
+)
+r=$?
+printf "subprocess \`%s' exited with $r\\n" "$1" >&2
+exit "$r"
