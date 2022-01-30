7ed2d16065f10dbd487f7d2d748341bd29ffe6e7
Yuta Aoyagi <yuta-aoyagi@users.noreply.github.com>
2022-01-30 18:09:20 +0900

    Import try-qemu/openwrt/driver.rb
    try-qemu/openwrt/driver.rb was modified at 2022-01-14 09:21:05.960262200 +0900.

---
+++ try-qemu/openwrt/driver.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/ruby -W
+
+require "English"
+require "socket"
+
+module Driver # TODO: find a unique name
+  class <<self
+    attr_accessor :srv, :todo
+  end
+end
+
+def do_work(driver, port)
+  driver.srv = TCPServer.new "::1", port
+
+  require "irb"
+  IRB.strat __FILE__
+end
+
+do_work Driver, 4444 if $PROGRAM_NAME == __FILE__
