bd29973bed0b531b6a9a2f2290ef0e5f54527e67
Yuta Aoyagi <yuta-aoyagi@users.noreply.github.com>
2022-01-30 23:34:25 +0900

    Temporary commit: add `.gsub('\r', "\r")`
    try-qemu/openwrt/vm_driver.rb was modified at 2022-01-16 14:30:56.522176200 +0900.

--- try-qemu/openwrt/vm_driver.rb
+++ try-qemu/openwrt/vm_driver.rb
@@ -47,7 +47,7 @@ def recv_thread(sock, logger, kernel)
   logger.info sock.expect(/press enter to activate this console/i, 120)
   while !sock.closed? && !sock.eof?
     kernel.sleep 0.1
-    s = sock.readpartial(1024).inspect.gsub '\n', "\n"
+    s = sock.readpartial(1024).inspect.gsub('\r', "\r").gsub '\n', "\n"
     logger.debug s
   end
   scok.close
