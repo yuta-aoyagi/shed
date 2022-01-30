901b5ee4f94e74ef67409f76fd0e433518482e6b
Yuta Aoyagi <yuta-aoyagi@users.noreply.github.com>
2022-01-30 23:12:46 +0900

    Accept a connection to dump what to receive
    try-qemu/openwrt/vm_driver.rb was modified at 2022-01-16 14:30:56.522176200 +0900.

--- try-qemu/openwrt/vm_driver.rb
+++ try-qemu/openwrt/vm_driver.rb
@@ -11,18 +11,32 @@ module VMDriver
   end
 end
 
+ACCEPTED = "accepted".freeze
+
 def create_logger(err_out)
   Logger.new err_out
 end
 
-def do_work(vm_driver, port, err_out)
+def recv_thread(sock, logger, kernel)
+  while !sock.closed? && !sock.eof?
+    kernel.sleep 0.1
+    s = sock.readpartial(1024).inspect.gsub '\n', "\n"
+    logger.debug s
+  end
+end
+
+def do_work(vm_driver, port, err_out, kernel)
   l = create_logger err_out
   l.info "start"
-  vm_driver.srv = TCPServer.new "::1", port
+  vm_driver.srv = srv = TCPServer.new "::1", port
+  s = srv.accept
+  l.info ACCEPTED
+  th = Thread.new { recv_thread s, l, kernel }
+  vm_driver.todo = { :l => l, :s => s, :th => th }
 
   IRB.start __FILE__
 
   l.info "quit"
 end
 
-do_work VMDriver, 4444, $stderr if $PROGRAM_NAME == __FILE__
+do_work VMDriver, 4444, $stderr, Kernel if $PROGRAM_NAME == __FILE__
