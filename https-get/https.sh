c7146caec09a160a711757c2712b7af1df99a305
Yuta Aoyagi <yuta-aoyagi@users.noreply.github.com>
2022-07-16 19:31:50 +0900

    Add some documentation comment to https-get files
    https-get/Makefile was modified at 2022-07-16 16:46:35.955187200 +0900.

--- https-get/https.sh
+++ https-get/https.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+# @(#)https.sh: a very tiny HTTPS client
+
+# usage: ./https.sh "${host}" "${method} ${request_uri}" ${optional_rest_args}
+# optional_rest_args are passed to the underlying `openssl s_client` command.
+# HTTP response goes to stdout; s_client's messages goes to stderr.
+
+# This script is developed for a pretty old Linux environment. Here are failed
+# tries:
+# - the certificate store is so old that it lacks ISRG Root X1
+# - openssl extension in Ruby 1.8.7p352 supports TLS 1.2 but not SNI
+# - curl 7.19.7 doesn't support TLS 1.2
+# - Wget 1.12 doesn't support SNI
+#
+# cf. https://twitter.com/yuuta_aoyagi/status/1538493143720787968
+
+# Limitation: when the HTTP response isn't completely received until `sleep 5`
+# finishes, the script's behavior is undefined.
+
 set -e
 
 hos=${1:?missing host}
