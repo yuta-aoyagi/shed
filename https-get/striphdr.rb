f8bdc21e2679575f5fadb99eeb68ef9067eddda7
Yuta Aoyagi <yuta-aoyagi@users.noreply.github.com>
2022-07-23 20:37:31 +0900

    Add step5 to remove HTTP response headers
    https-get/striphdr.rb was modified at 2022-06-19 20:49:04 +0900.

---
+++ https-get/striphdr.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby -W
+
+LFCRLF = [10, 13, 10]
+
+def do_work(input, output)
+  a = input.each_byte.to_a
+  raise unless (i = a.each_cons(3).find_index(LFCRLF))
+  output << a[(i + 3)..-1].pack("C*")
+end
+
+do_work ARGF, $stdout if $PROGRAM_NAME == __FILE__
