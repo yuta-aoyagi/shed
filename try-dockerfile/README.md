0efb29ab4047ece65c714371f49de94abee805b7
Yuta Aoyagi <yuta-aoyagi@users.noreply.github.com>
2022-08-01 02:06:19 +0900

    Generalize docker-gpgpdump.sh and add the platform version to tag name

--- try-dockerfile/README.md
+++ try-dockerfile/README.md
@@ -3,3 +3,4 @@
 How to build:
 
 	VERSION_IN_TAG=2.2.35 ALPINE_VERSION=3.16.1 sh docker-gnupg.sh
+	VERSION=0.14.0 UBI_VERSION=8.6-394 ALPINE_VERSION=3.16.1 sh docker-gpgpdump.sh
--- try-dockerfile/docker-gpgpdump.sh
+++ try-dockerfile/docker-gpgpdump.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker build -t gpgpdump:0.14.0 --build-arg ALPINE_VERSION=3.16.1 --build-arg VERSION=0.14.0 --build-arg UBI_VERSION=8.6-394 - <Dockerfile-gpgpdump
+docker build -t "gpgpdump:${VERSION?}-ubi8-micro${UBI_VERSION?}" --build-arg ALPINE_VERSION --build-arg VERSION --build-arg UBI_VERSION - <Dockerfile-gpgpdump
