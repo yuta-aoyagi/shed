70af29f0f4745d69065776fc7c26026605590266
Yuta Aoyagi <yuta-aoyagi@users.noreply.github.com>
2023-01-31 20:49:47 +0900

    Generalize Dockerfile-gpgpdump & docker-gpgpdump.sh to allow UBI 9
    Files' mtime:
    2023-01-22 11:44:56.999845800 +0900 try-dockerfile/Dockerfile-gpgpdump
    2023-01-23 08:47:51.126785400 +0900 try-dockerfile/docker-gpgpdump.sh

--- try-dockerfile/docker-gpgpdump.sh
+++ try-dockerfile/docker-gpgpdump.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-docker build -t "gpgpdump:${VERSION?}-ubi8-micro${UBI_VERSION?}" --build-arg ALPINE_VERSION --build-arg VERSION --build-arg UBI_VERSION - <Dockerfile-gpgpdump
+
+# Some other projects (autoconf & shunit2) say `` is more portable than $().
+# shellcheck disable=2006
+
+major_version() {
+  printf %s\\n "$1" | sed 's/\..*//'
+}
+
+BASE=ubi`major_version "${UBI_VERSION?}"`-micro
+docker build -t "gpgpdump:${VERSION?}-$BASE$UBI_VERSION" --build-arg ALPINE_VERSION --build-arg VERSION --build-arg BASE_IMAGE="redhat/${BASE}:$UBI_VERSION" - <Dockerfile-gpgpdump
