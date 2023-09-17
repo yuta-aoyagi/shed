d7db9692c02e2b4d64877341368113351e43ce11
Yuta Aoyagi <yuta-aoyagi@users.noreply.github.com>
2023-09-17 22:30:02 +0900

    Add .rspec & spec/spec_helper.rb
    Files' mtime:
    2023-09-17 21:13:39.093253800 +0900 try-qemu/openwrt-sep-2023/.rspec
    2023-09-17 22:25:13.338729400 +0900 try-qemu/openwrt-sep-2023/spec/spec_helper.rb

---
+++ try-qemu/openwrt-sep-2023/spec/spec_helper.rb
@@ -0,0 +1,33 @@
+# Given that it is always loaded, you are encouraged to keep this file as
+# light-weight as possible. Requiring heavyweight dependencies from this file
+# will add to the boot time of your test suite on EVERY test run, even for an
+# individual file that may not need all of that loaded. Instead, consider making
+# a separate helper file that requires the additional dependencies and performs
+# the additional setup, and require it from the spec files that actually need
+# it.
+#
+# See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+  end
+
+  config.mock_with :rspec do |mocks|
+  end
+
+=begin
+  # This setting enables warnings. It's recommended, but in some cases may
+  # be too noisy due to issues in dependencies.
+  config.warnings = true
+
+  # Print the 10 slowest examples and example groups at the
+  # end of the spec run, to help surface which specs are running
+  # particularly slow.
+  config.profile_examples = 10
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = :random
+=end
+end
