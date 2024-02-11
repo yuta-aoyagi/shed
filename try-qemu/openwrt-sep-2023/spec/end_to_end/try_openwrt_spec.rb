6efb503962444266ac0baa9ee60305a72b490b31
Yuta Aoyagi <yuta-aoyagi@users.noreply.github.com>
2024-02-11 15:55:31 +0900

    Add the second test checking the Makefile default target

--- try-qemu/openwrt-sep-2023/spec/end_to_end/try_openwrt_spec.rb
+++ try-qemu/openwrt-sep-2023/spec/end_to_end/try_openwrt_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "tempfile"
+require "tmpdir"
 
 RSpec.describe "try_openwrt's Makefile" do
   it "has a target `help`" do
@@ -8,6 +9,19 @@ RSpec.describe "try_openwrt's Makefile" do
       err.close
       output = `make help 2>#{err.path}`
       expect(output).to match(/usage.*make /)
+    end
+  end
+
+  describe "default target" do
+    it "creates a disk image" do
+      pending
+      Dir.mktmpdir nil, "." do |dir|
+        Tempfile.open(["", ".qcow2"], ".") do |img|
+          img.close
+          system "make DISK=#{img.path} >#{dir}/out 2>#{dir}/err"
+          expect(img.size).to be > 1000
+        end
+      end
     end
   end
 end
